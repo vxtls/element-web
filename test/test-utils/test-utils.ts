882c52248fbda95bc24cf15bb84ea14ba52704b9
Florian Duros <florian.duros@ormaz.fr>
2025-01-23 15:24:01 +0100

    test(dev tools): add tests for crypto component

--- test/test-utils/test-utils.ts
+++ test/test-utils/test-utils.ts
@@ -151,6 +151,8 @@ export function createTestClient(): MatrixClient {
                 },
             }),
             isCrossSigningReady: jest.fn().mockResolvedValue(false),
+            getSessionBackupPrivateKey: jest.fn().mockResolvedValue(null),
+            isSecretStorageReady: jest.fn().mockResolvedValue(false),
         }),
 
         getPushActionsForEvent: jest.fn(),
